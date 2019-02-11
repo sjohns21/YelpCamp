876b4bf74b9f5e06fad6a7611260da23df5e5b65
Steve Johnson <steve21johnson@gmail.com>
2019-02-10 18:42:49 -0800

    Campground Show page

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -21,7 +22,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //   {
 //     name: "Granite Hill",
 //     image:
-//       "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+//       "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+//     description: "this is a huge granite hill"
 //   },
 //   function(err, campground) {
 //     if (err) {
@@ -33,68 +35,85 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //   }
 // );
 
-var campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://i1.wp.com/visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg?fit=640%2C480"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image:
-      "https://photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f9c77ca3eab7ba_960.jpg&user=Pexels"
-  },
-  {
-    name: "Salmon Creek",
-    image:
-      "https://i1.wp.com/visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg?fit=640%2C480"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image:
-      "https://photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f9c77ca3eab7ba_960.jpg&user=Pexels"
-  }
-];
+// var campgrounds = [
+//   {
+//     name: "Salmon Creek",
+//     image:
+//       "https://i1.wp.com/visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg?fit=640%2C480"
+//   },
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+//   },
+//   {
+//     name: "Mountain Goat's Rest",
+//     image:
+//       "https://photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f9c77ca3eab7ba_960.jpg&user=Pexels"
+//   },
+//   {
+//     name: "Salmon Creek",
+//     image:
+//       "https://i1.wp.com/visitmckenzieriver.com/oregon/wp-content/uploads/2015/06/paradise_campground.jpg?fit=640%2C480"
+//   },
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+//   },
+//   {
+//     name: "Mountain Goat's Rest",
+//     image:
+//       "https://photosforclass.com/download/pixabay-1845719?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db50a2ff5083ed1584d05fb1d4e97e07ee3d21cac104491f9c77ca3eab7ba_960.jpg&user=Pexels"
+//   }
+// ];
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
   // get all campgrounds from db
   Campground.find({}, function(err, AllCampgrounds) {
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds", { campgrounds: AllCampgrounds });
+      res.render("index", { campgrounds: AllCampgrounds });
     }
   });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
   res.render("new.ejs");
 });
 
+//CREATE - add new campground to db
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
+  var desc = req.body.description;
+  var newCampground = { name: name, image: image, description: desc };
   //create new campground and save to db
   Campground.create(newCampground, function(err, newlyCreated) {
     if (err) {
       console.log(err);
     } else {
       res.redirect("/campgrounds");
+    }
+  });
+});
+
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+  //find the campground with provided ID
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      res.render("show", { campground: foundCampground });
     }
   });
 });
