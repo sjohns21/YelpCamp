96a27794f72bfac576b28c4f96ce95419939dac9
Steve Johnson <steve21johnson@gmail.com>
2019-02-10 17:40:46 -0800

    adding mongoose

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", {
+  useNewUrlParser: true
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+//   },
+//   function(err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("campground");
+//       console.log(campground);
+//     }
+//   }
+// );
 
 var campgrounds = [
   {
@@ -43,7 +71,14 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  // get all campgrounds from db
+  Campground.find({}, function(err, AllCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: AllCampgrounds });
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res) {
@@ -54,8 +89,14 @@ app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  //create new campground and save to db
+  Campground.create(newCampground, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.listen(3000);
