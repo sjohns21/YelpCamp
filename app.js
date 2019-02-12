e13cee3e0e9401956f0077fdc7af6d23574fe8bd
Steve Johnson <steve21johnson@gmail.com>
2019-02-12 15:20:14 -0800

    auth: logging in

--- app.js
+++ app.js
@@ -145,4 +145,19 @@ app.post("/register", function(req, res) {
   });
 });
 
+//show login form
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+//handle login logic
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
+
 app.listen(3000);
