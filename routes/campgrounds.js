a9214e860e1d1ad977c4893453b8ceec205a631f
Steve Johnson <steve21johnson@gmail.com>
2019-02-13 14:29:10 -0800

    campground authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -62,13 +62,9 @@ router.get("/:id", function(req, res) {
 });
 
 // EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
   Campground.findById(req.params.id, function(err, foundCampground) {
-    if (err) {
-      res.redirect("/campgrounds");
-    } else {
-      res.render("campgrounds/edit", { campground: foundCampground });
-    }
+    res.render("campgrounds/edit", { campground: foundCampground });
   });
 });
 
@@ -107,4 +103,22 @@ function isLoggedIn(req, res, next) {
   res.redirect("/login");
 }
 
+function checkCampgroundOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        //does user own campground?
+        if (foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
 module.exports = router;
