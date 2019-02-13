2272679e02a52a4a0c592ee1d3bde05bca981139
Steve Johnson <steve21johnson@gmail.com>
2019-02-13 13:29:17 -0800

    campground edit and update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -73,6 +73,31 @@ router.get("/:id/edit", function(req, res) {
 });
 
 // UPDATE CAMPGROUND ROUTE
+router.put("/:id", function(req, res) {
+  // find and update the correct campground
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(
+    err,
+    updatedCampground
+  ) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+  // redirect somewhere
+});
+
+//DESTROY CAMPGROUND ROUTE
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
 
 //middleware
 function isLoggedIn(req, res, next) {
