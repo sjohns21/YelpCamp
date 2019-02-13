952a1c6aa2827f00449d74a79f60f06b6ce7a2bd
Steve Johnson <steve21johnson@gmail.com>
2019-02-13 15:57:37 -0800

    comment edit and update

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,34 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
+//COMMENT EDIT
+router.get("/:comment_id/edit", function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {
+        campground_id: req.params.id,
+        comment: foundComment
+      });
+    }
+  });
+});
+
+//COMMENT UPDATE
+router.put("/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(
+    err,
+    updatedComment
+  ) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 //middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
